7b9c5009350c17861462c1a08ea477c4411f86b7
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-12-13 20:40:08 +0300

    Задание 3 выполнено

--- materials/views.py
+++ materials/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Course, Lesson
 from materials.serializers import (
@@ -6,11 +7,10 @@ from materials.serializers import (
     LessonSerializer,
     CourseCountSerializer,
 )
-from users.permissions import IsModer
+from users.permissions import IsModer, IsOwner
 
 
 class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
 
     def get_serializer_class(self):
         if self.action == "retrieve":
@@ -23,17 +23,25 @@ class CourseViewSet(viewsets.ModelViewSet):
         course.save()
 
     def get_permissions(self):
-        if self.action in ["create", "destroy"]:
+        if self.action == "create":
             self.permission_classes = [~IsModer]
-        else:
-            self.permission_classes = [IsModer]
+        elif self.action == "destroy":
+            self.permission_classes = [~IsModer & IsOwner]
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = [IsModer | IsOwner]
         return super().get_permissions()
+
+    def get_queryset(self):
+        if IsModer in self.permission_classes:
+            return Course.objects.all()
+        else:
+            return Course.objects.filter(owner=self.request.user.id)
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = [~IsModer]
+    permission_classes = [~IsModer, IsAuthenticated]
 
     def perform_create(self, serializer):
         lesson = serializer.save()
@@ -42,21 +50,28 @@ class LessonCreateAPIView(generics.CreateAPIView):
 
 
 class LessonListAPIView(generics.ListAPIView):
-    queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+    def get_queryset(self):
+        if IsModer in self.permission_classes:
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=self.request.user.id)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = [~IsModer]
+    permission_classes = (IsAuthenticated, IsOwner, ~IsModer)
--- users/permissions.py
+++ users/permissions.py
@@ -8,11 +8,7 @@ class IsModer(BasePermission):
         return request.user.groups.filter(name="moders").exists()
 
 
-class IsOwnerOrReadOnly(BasePermission):
+class IsOwner(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if obj.owner:
-            return True
-
-        # Instance must have an attribute named `owner`.
         return obj.owner == request.user
